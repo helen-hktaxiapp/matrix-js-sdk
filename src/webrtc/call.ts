dacc2f26438b8e19e0108992a42ca45700ecaefe
helennhl <helen@hktaxiapp.com>
2021-05-11 17:47:32 +0800

    Uncomment updatemutestatus

--- src/webrtc/call.ts
+++ src/webrtc/call.ts
@@ -1031,10 +1031,10 @@ export class MatrixCall extends EventEmitter {
             // playing music etc. to the other side.
             tranceiver.direction = onHold ? 'inactive' : 'sendrecv';
         }
-        // this.updateMuteStatus();
+        this.updateMuteStatus();
 
         if (!onHold) {
-            this.playRemoteAudio();
+            // this.playRemoteAudio();
             if(this.recorderState != RecorderState.Recording){
                 this.rtcRecorder.resume();
                 this.recorderState = RecorderState.Recording;
